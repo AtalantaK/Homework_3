60aa199aa725dd3e86ef1caddd3a541583e45366
kska0221 <aksinia.ra@gmail.com>
2025-05-08 10:42:32 +0500

    Homework 3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Task 1
+        String[] todoList = new String[5];
+        todoList[0] = "Приготовить ужин";
+        todoList[1] = "Купить продукты";
+        todoList[2] = "Покормить кота";
+        todoList[3] = "Убрать квартиру";
+        todoList[4] = "Сходить на работу";
+
+
+        //Task 2
+        double[] arrayDouble = new double[3];
+        arrayDouble[0] = Math.round(Math.PI * 100_000.0) / 100_000.0;
+        //String str = String.format("%.5f",Math.PI);
+        arrayDouble[1] = Math.round(Math.E * 100_000.0) / 100_000.0;
+        arrayDouble[2] = 1.0;
+
+        //Task 3
+
+        //Task 4
+
+        //Task 5
+
+        //Task 6
+    }
+}
