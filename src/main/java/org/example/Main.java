a72f38f67ae5ce1f062bac0569b298a19379a5e8
kska0221 <aksinia.ra@gmail.com>
2025-05-08 20:46:20 +0500

    Homework 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,16 +16,32 @@ public class Main {
         //Task 2
         double[] arrayDouble = new double[3];
         arrayDouble[0] = Math.round(Math.PI * 100_000.0) / 100_000.0;
-        //String str = String.format("%.5f",Math.PI);
         arrayDouble[1] = Math.round(Math.E * 100_000.0) / 100_000.0;
         arrayDouble[2] = 1.0;
 
         //Task 3
+        Movie[] films = new Movie[3];
+        films[0] = new Movie("Фильм 1", 7.0, "Ужасы", "США", false, "1998");
+        films[1] = new Movie("Фильм 2", 2.5, "Комедия", "Россия", false, "2015");
+        films[2] = new Movie("Фильм 3", 5.8, "Фантастика", "Великобритания", true, "2001");
 
         //Task 4
+        for (int i = 1; i <= 1_000_000_000; i++) {
+            System.out.println(i);
+        }
 
         //Task 5
+        for (Movie film : films) {
+            System.out.println(film.getYearMovie() + " - "
+                    + film.getNameMovie() + " - "
+                    + film.getGenreMovie() + " - "
+                    + film.getRateMovie());
+        }
 
         //Task 6
+        String[] arrayString = {"Михаил", "Сергей", "Юрий", "Рустам", "Андрей", "Владимир", "Амир", "Олег", "Евгений", "Павел"};
+        for (int i = 0; i < 3; i++) {
+            System.out.println(arrayString[i]);
+        }
     }
 }
